bf91acea036f5be5bddde90b2f37aacfe9921f6d
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-03 18:21:37 +0530

    Update skip.py
    from InflexMusic.utils.thumbnails import get_thumb

--- InflexMusic/plugins/admins/skip.py
+++ InflexMusic/plugins/admins/skip.py
@@ -9,7 +9,6 @@ from InflexMusic.utils.database import get_loop
 from InflexMusic.utils.decorators import AdminRightsCheck
 from InflexMusic.utils.inline import close_markup, stream_markup
 from InflexMusic.utils.stream.autoclear import auto_clean
-from InflexMusic.utils.thumbnails import get_thumb
 from config import BANNED_USERS
 
 
